2fc1f633645202d00fb19d290ca0ac4dab07d72d
Cosolord <cosolord@gmail.com>
2017-10-29 07:10:49 +0100

    Add GET todos ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require("./models/todo.js");
@@ -25,6 +26,23 @@ app.get("/todos", (req, res) => {
     res.send({todos : todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get("/todos/:id", function(req, res) {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((foundTodo) => {
+    if(!foundTodo){
+      res.status(404).send();
+    }
+    res.send({foundTodo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
